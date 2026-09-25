af28c67a46d82e0274035968d7fd55dd0eac5295
Mudit Jain <muditjain2204@gmail.com>
2026-09-25 15:19:20 +0000

    feat(webhook): add clerk webhook endpoint for processing raw JSON requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,12 +8,14 @@ import cors from "cors";
 import fs from "fs";
 import path from "path";
 import  job  from "./lib/cron";
-
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 const app = express();
 const PORT = process.env.PORT || 3001;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use("/api/webhooks/clerk", express.raw({type:"application/json"}),clerkWebhook)
 console.log(process.env.DB_URL);
 
 app.use(express.json());
